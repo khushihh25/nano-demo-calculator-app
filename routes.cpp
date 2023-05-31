b2d97348defcd83ed8346f6fdb3ff23869cb9d08
khushihh25 <khushiagarwal220@gmail.com>
2023-05-31 23:42:55 +0530

    Khushi

--- routes.cpp
+++ routes.cpp
@@ -2,15 +2,25 @@
 
 crow::response greet()
 {
-    return crow::response{""};
+    return crow::response{"Hello world!"};
 }
 crow::response add(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    int first = input["first"].i();
+    int second = input["second"].i();
+    int result = first + second;
+    crow::json::wvalue responseJson;
+    responseJson["result"] = result;
+    return crow::response{ responseJson };
 }
 crow::response subtract(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    int first = input["first"].i();
+    int second = input["second"].i();
+    int result = first - second;
+    crow::json::wvalue responseJson;
+    responseJson["result"] = result;
+    return crow::response{ responseJson };
 }
